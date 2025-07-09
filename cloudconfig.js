cb7e7df406109c122220b342132f93d5ab6594dc
Vineet Waman <wamanvinit8717@gmail.com>
2025-07-09 21:20:03 +0530

    Add Cloudinary image upload and listing creation

--- cloudconfig.js
+++ cloudconfig.js
@@ -1,3 +1,38 @@
+// const cloudinary = require('cloudinary').v2;
+// const { CloudinaryStorage } = require('@fluidjs/multer-cloudinary');
+
+// cloudinary.config({
+//   cloud_name: process.env.CLOUD_NAME,
+//   api_key: process.env.API_KEY,
+//   api_secret: process.env.API_SECRET
+// });
+
+// const storage = new CloudinaryStorage({
+//   cloudinary: cloudinary,
+//   params: {
+//     folder: 'wanderlust_dev',
+//      allowed_formats: ['jpg', 'png', 'jpeg'],
+  
+//   },
+// });
+// const cloudinary = require('cloudinary').v2;
+// const multer = require('multer');
+// const { multerCloudinary } = require('@fluidjs/multer-cloudinary');
+
+// cloudinary.config({
+//    cloud_name: process.env.CLOUD_NAME,
+//   api_key: process.env.API_KEY,
+//   api_secret: process.env.API_SECRET
+// });
+
+// const upload = multer({
+//   storage: multerCloudinary(cloudinary, {
+//     folder: 'Wanderlust',
+//     allowedFormats: ['jpeg', 'png', 'jpg'],
+//   }),
+// });
+
+// module.exports={cloudinary,storage};
 const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('@fluidjs/multer-cloudinary');
 
@@ -11,8 +46,15 @@ const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: {
     folder: 'wanderlust_dev',
-     allowed_formats: ['jpg', 'png', 'jpeg'],
-  
+    allowed_formats: ['jpg', 'png', 'jpeg'],
   },
 });
-module.exports={cloudinary,storage};
+
+const multer = require('multer');
+const upload = multer({ storage });
+
+module.exports = {
+  cloudinary,
+  storage,
+  upload
+};
