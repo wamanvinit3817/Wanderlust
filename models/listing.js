b82f4202503e08ef9678f1d92049ef0771c569a4
Vineet Waman <wamanvinit8717@gmail.com>
2025-07-09 22:15:23 +0530

    Final working version with Cloudinary and Mapbox integration

--- models/listing.js
+++ models/listing.js
@@ -9,11 +9,10 @@ const listingSchema=new schema({
     description:{
         type:String
     },
-    image:{
-        type:String,
-        default:"https://images.unsplash.com/photo-1494526585095-c41746248156?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        set:(v)=> v==="" ? "https://images.unsplash.com/photo-1494526585095-c41746248156?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D":v,
-    },
+    image: {
+  url: String,
+  filename: String
+},
     price:{
         type:Number
     },
